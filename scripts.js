5e25ba4c99c0e2873c8d51e3215c3a6f32dcca8a
Coolio-McDoodle <jordandawes1997@hotmail.co.uk>
2015-07-07 10:40:21 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,30 @@
+function writeNewline0(){document.write("<br/>")}
+document.write("Wake up in the morning feeling like P-diddy")
+writeNewline0();
+document.write("<br/>Penguins are cool as long as you don't hit them with rocks")
+writeNewline0();
+function becomeBatman(imageEarl) {
+writeNewline0;
+	document.write("imageEarl"+ imageEarl)
+} 
+becomeBatman("http://i.imgur.com/dUboRcM.png")
+function killtheBatman(ImageEarl) {
+        document.write("<img src=\""+ImageEarl+"\"/>")
+}
+writeNewline0;		
+killtheBatman("http://i.imgur.com/dUboRcM.png");
+var numberA = 39;
+var numberB = 26;
+writeNewline0;
+document.write("The total is: " + (numberA + numberB)); 
+var currentTotal = 0;
+function runningTotal(numberToAdd)
+{
+    var currentTotal = 0;
+    currentTotal = currentTotal + numberToAdd;
+    document.write("The current total is: " + currentTotal);
+}
+runningTotal(12);
+runningTotal(448);
+runningTotal(90);
+runningTotal(1253);
