c57efc443d63d9c661c6b86f80a2bfae3df66704
Coolio-McDoodle <jordandawes1997@hotmail.co.uk>
2015-07-08 14:12:33 +0100

    pol

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ var character;
 function preload()
 {
     game.load.tilemap("Level", "level.json", null, Phaser.Tilemap.TILED_JSON);
-    game.load.image("Tiles", "tiles-small.png");    
+    game.load.image("Tiles", "images/tiles-small.png"); 
     game.load.spritesheet("Character", "images/My character.png", 92, 92, 24);
 }    
 function create()
@@ -12,7 +12,10 @@ function create()
     map.addTilesetImage("Tiles"); 
     var layer = map.createLayer( 0 );
     layer.resizeWorld();   
-character= game.add.sprite(100, 100, "Character");  
+character= game.add.sprite ( 4*64, 11*64, "Character" );   
+game.camera.y = 600;
+character.anchor.x = 0.5;
+    character.anchor.y = 1;
 }
 function update()
 {
@@ -21,4 +24,3 @@ function update()
      if (game.input.keyboard.isDown(Phaser.Keyboard.A))
     character.x = character.x - 1;
 }
-game.camera.y = 600;
