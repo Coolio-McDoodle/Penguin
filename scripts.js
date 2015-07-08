8c8093fb561937f0ed3175615e602c49bfca1ef7
Coolio-McDoodle <jordandawes1997@hotmail.co.uk>
2015-07-08 11:07:52 +0100

    hey dere

--- scripts.js
+++ scripts.js
@@ -3,13 +3,13 @@ var character;
 function preload()
 {
     game.load.tilemap("Level", "level.json", null, Phaser.Tilemap.TILED_JSON);
-    game.load.image("TILE_IMAGE_NAME", "images/my map.tmx.png");    
+    game.load.image("Tiles", "images/my map.tmx.png");    
     game.load.spritesheet("Character", "images/My character.png", 92, 92, 24);
 }    
 function create()
 {
     var map = game.add.tilemap("Level", 196, 196);
-    map.addTilesetImage("map.tmx.png"); 
+    map.addTilesetImage("Tiles.png"); 
     var layer = map.createLayer( 0 );
     layer.resizeWorld();   
 character= game.add.sprite(100, 100, "Character");  
