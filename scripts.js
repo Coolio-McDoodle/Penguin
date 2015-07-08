4d6fb41659f7617ade66fe16a4f558eba908bb0a
Coolio-McDoodle <jordandawes1997@hotmail.co.uk>
2015-07-08 10:54:52 +0100

    Indent

--- scripts.js
+++ scripts.js
@@ -2,26 +2,22 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, "my-game", { preload: preload,
 var character;
 function preload()
 {
-game.load.tilemap("Level", "level.json", null, Phaser.Tilemap.TILED_JSON);
-game.load.image("TILE_IMAGE_NAME", "images/my map.tmx.png");    
-}
-{
-game.load.spritesheet("Character", "images/My character.png", 92, 92, 24);
-}
+    game.load.tilemap("Level", "level.json", null, Phaser.Tilemap.TILED_JSON);
+    game.load.image("TILE_IMAGE_NAME", "images/my map.tmx.png");    
+    game.load.spritesheet("Character", "images/My character.png", 92, 92, 24);
+}    
 function create()
 {
-var map = game.add.tilemap("Level", 196, 196);
-map.addTilesetImage("map.tmx.png"); 
-var layer = map.createLayer( 0 );
-layer.resizeWorld();   
-}
-{
+    var map = game.add.tilemap("Level", 196, 196);
+    map.addTilesetImage("map.tmx.png"); 
+    var layer = map.createLayer( 0 );
+    layer.resizeWorld();   
 character= game.add.sprite(100, 100, "Character");  
 }
 function update()
 {
-if (game.input.keyboard.isDown(Phaser.Keyboard.D))
-character.x = character.x + 1;
- if (game.input.keyboard.isDown(Phaser.Keyboard.A))
-character.x = character.x - 1;
+    if (game.input.keyboard.isDown(Phaser.Keyboard.D))
+    character.x = character.x + 1;
+     if (game.input.keyboard.isDown(Phaser.Keyboard.A))
+    character.x = character.x - 1;
 }
