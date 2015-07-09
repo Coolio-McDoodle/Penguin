484e68b63b08917260c07969a91bca06d3332b4e
Coolio-McDoodle <jordandawes1997@hotmail.co.uk>
2015-07-09 11:47:09 +0100

    top kek

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, "my-game", { preload: preload, create: create, update: update });
 var character;
+var layer;
 function preload()
 {
     game.load.tilemap("Level", "level.json", null, Phaser.Tilemap.TILED_JSON);
@@ -8,19 +9,34 @@ function preload()
 }    
 function create()
 {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     var map = game.add.tilemap("Level", 64, 64);
+     map.setCollisionBetween(4, 15);
     map.addTilesetImage("Tiles"); 
-    var layer = map.createLayer( 0 );
+    layer = map.createLayer( 0 );
     layer.resizeWorld();   
-character= game.add.sprite ( 4*64, 11*64, "Character" );   
-game.camera.y = 600;
+character = game.add.sprite(4*64, 12*64 + 6, "Character");
+game.physics.enable(character);
+character.body.setSize(32, 58, 4, -8);
+character.animations.add("idle", [5]);   
 character.anchor.x = 0.5;
     character.anchor.y = 1;
+    character.animations.play("idle")
+    game.camera.follow(character);
 }
 function update()
 {
+    game.physics.arcade.collide(character, layer);
+    character.body.gravity.y = 300;
+        character.body.velocity.x = 0;
     if (game.input.keyboard.isDown(Phaser.Keyboard.D))
-    character.x = character.x + 1;
+   character.body.velocity.x = 2 * 60;
      if (game.input.keyboard.isDown(Phaser.Keyboard.A))
-    character.x = character.x - 1;
+    character.x = character.x - 2;
+     if (game.input.keyboard.isDown(Phaser.Keyboard.W))
+    {
+        if (character.body.onFloor())
+        {
+            character.body.velocity.y = -200;
+        }
 }
